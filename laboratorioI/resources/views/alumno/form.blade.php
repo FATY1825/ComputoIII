63ec0bf26210f41b0a5ef260b2e6b96f5e922843
ftaya <ftayala89@gmail.com>
2023-05-19 12:28:48 -0600

    Cambios

--- laboratorioI/resources/views/alumno/form.blade.php
+++ laboratorioI/resources/views/alumno/form.blade.php
@@ -6,6 +6,11 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
     <title>Formulario de registro</title>
+    <style>
+        body {
+            background-image: linear-gradient(90deg, #d53369 0%, #daae51 100%);
+        }
+    </style>
 </head>
 <body>
     <div class="container">
